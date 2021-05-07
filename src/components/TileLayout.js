9440b7e7d017ff0eca4720fa38ac5bea0bfb0e03
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-05-07 16:10:14 -0700

    Add rough TileLayout.js

---
+++ src/components/TileLayout.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { TILE_RATIO, WORLD_SIZE } from '../constants/constants';
+import Tile from './Tile';
+// import tile images from '../images'
+
+const TileLayout = () => {
+  const tiles = [
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    ]
+  return (
+   // tiles.map
+)}
+
+export default TileLayout;
