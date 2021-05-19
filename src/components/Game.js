1078264b55020afe2fcd52b82a9e82c32bee1e97
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-05-19 11:36:59 -0700

    Begin working on a intro screen

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,12 @@
 import React from "react";
 import World from "./World";
 import Inputs from "./Inputs";
+import StartScreen from './StartScreen';
 
 const Game = () => {
   return(
     <>
+      <StartScreen/>
       <World/>
       <Inputs/>
     </>
